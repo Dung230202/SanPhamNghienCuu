41567aa47ed299769206dbdfed7ed3fefb038170
Dung230202 <dungsp2302@gmail.com>
2024-01-17 19:43:04 +0700

    reset

--- learn/body_action.js
+++ learn/body_action.js
@@ -1,75 +1,96 @@
+// Định nghĩa một đối tượng chứa các giá trị CSS phụ cho các hành động
 var extraCSS = { "sort": "65px", "create": "92px" };
-    var actionsIds = { "create": { "p": "Create(A)", "parents": ["create-sorted", "create-nearly-sorted", "create-many-duplicates"] }, "sort": { "p": "Sort", "onClick": "sortGeneric()" } };
-    var isOpens = {};
-    var len = 2;
-    var keys = ["create", "sort"];
-    for (i = 0; i < len; i++) {
-      var actionDetails = keys[i];
-      isOpens[actionDetails] = false;
-    }
-    function openAction(id) {
-      hideThirdTiers(id);
-      $("." + id).css("bottom", extraCSS[id]);
-      if (!isOpens[id]) {
-        $('.' + id).fadeIn('fast');
-        isOpens[id] = true;
-      }
-    }
-    function closeAction(id) {
-      hideThirdTiers(id);
-      if (isOpens[id]) {
-        $('.' + id).fadeOut('fast');
-        $('#' + id + '-err').html("");
-        isOpens[id] = false;
-      }
-    }
 
-    function hideEntireActionsPanel() {
-      //hideAllThirdTiers();
-      closeAction('create');
-      closeAction('sort');
-      hideActionsPanel();
+// Định nghĩa đối tượng chứa thông tin về các hành động và các thao tác tương ứng
+var actionsIds = {
+  "create": { "p": "Create(A)", "parents": ["create-sorted", "create-nearly-sorted", "create-many-duplicates"] },
+  "sort": { "p": "Sort", "onClick": "sortGeneric()" }
+};
+
+// Một đối tượng để theo dõi trạng thái mở/đóng của các hành động
+var isOpens = {};
+
+// Số lượng hành động
+var len = 2;
+
+// Mảng chứa các key của đối tượng actionsIds
+var keys = ["create", "sort"];
+
+// Khởi tạo trạng thái đóng cho tất cả các hành động
+for (i = 0; i < len; i++) {
+  var actionDetails = keys[i];
+  isOpens[actionDetails] = false;
+}
+
+// Hàm mở hành động
+function openAction(id) {
+  hideThirdTiers(id);
+  $("." + id).css("bottom", extraCSS[id]);
+  if (!isOpens[id]) {
+    $('.' + id).fadeIn('fast');
+    isOpens[id] = true;
+  }
+}
+
+// Hàm đóng hành động
+function closeAction(id) {
+  hideThirdTiers(id);
+  if (isOpens[id]) {
+    $('.' + id).fadeOut('fast');
+    $('#' + id + '-err').html("");
+    isOpens[id] = false;
+  }
+}
+
+// Hàm ẩn toàn bộ bảng điều khiển hành động
+function hideEntireActionsPanel() {
+  closeAction('create');
+  closeAction('sort');
+  hideActionsPanel();
+}
+
+// Hàm ẩn tất cả các lớp hành động cấp ba
+function hideThirdTiers(tier1Action) {
+  if ("parents" in actionsIds[tier1Action]) {
+    for (const secondTierAction of actionsIds[tier1Action]["parents"]) {
+      $('#' + secondTierAction + '-third-tier').fadeOut('fast');
     }
+  }
+}
 
-    function hideThirdTiers(tier1Action) {
-      if ("parents" in actionsIds[tier1Action]) {
-        for (const secondTierAction of actionsIds[tier1Action]["parents"]) {
-          $('#' + secondTierAction + '-third-tier').fadeOut('fast');
-        }
-      }
-    }
+// Hàm ẩn tất cả các lớp hành động cấp ba cho tất cả các hành động
+function hideAllThirdTiers() {
+  for (const tier1Action in actionsIds) {
+    hideThirdTiers(tier1Action);
+  }
+}
 
-    function hideAllThirdTiers() {
-      for (const tier1Action in actionsIds) {
-        hideThirdTiers(tier1Action);
-      }
-    }
+// Hàm chuyển đổi giữa các thêm/xóa các phần tử con
+function toggleChildExtras(tier1, selectedParentTier2) {
+  hideThirdTiers(tier1);
+  var style = extraCSS[tier1];
+  $("." + tier1).css("bottom", (parseInt(style.substring(0, style.length - 2)) - 27));
+  $('#' + selectedParentTier2 + '-third-tier').fadeIn('fast');
+}
 
-    function toggleChildExtras(tier1, selectedParentTier2) {
-      hideThirdTiers(tier1);
-      //this actionId must have childExtras
-      var style = extraCSS[tier1];
-      $("." + tier1).css("bottom", (parseInt(style.substring(0, style.length - 2)) - 27));
-      //fade ins and outs are done on the ID whereas css class is set on the class
-      //fade in the third tier of the selected 2nd tier parent
-      $('#' + selectedParentTier2 + '-third-tier').fadeIn('fast');
-    
-    }
+// Hàm khôi phục chiều cao ban đầu của hành động
+function setActionHeightOriginal(tier1Action) {
+  $("." + tier1Action).css("bottom", extraCSS[tier1Action]);
+}
 
-    function setActionHeightOriginal(tier1Action) {
-      $("." + tier1Action).css("bottom", extraCSS[tier1Action]);
-    }
+// Xử lý sự kiện khi người dùng nhấp vào nút 'create'
+$('#' + 'create').click(function () {
+  openAction('create');
+  closeAction('sort');
+});
 
-    $('#' + 'create').click(function () {
-      openAction('create');
-      closeAction('sort');
-    });
-    $('#' + 'sort').click(function () {
-      openAction('sort');
-      closeAction('create');
-    });
+// Xử lý sự kiện khi người dùng nhấp vào nút 'sort'
+$('#' + 'sort').click(function () {
+  openAction('sort');
+  closeAction('create');
+});
 
-    //important to have all the input fields in actions menu take an id containing '-input'
-    $("[id*='-input']").on("keypress keydown keyup", (event) => {
-      event.stopPropagation();
-    });
+// Chặn sự kiện lan truyền từ các trường nhập liệu trong menu hành động
+$("[id*='-input']").on("keypress keydown keyup", (event) => {
+  event.stopPropagation();
+});
